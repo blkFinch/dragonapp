9c850acf11ecd822af3a6401b4d4cc609ba7f55f
N. Hutchison <blkfinch@gmail.com>
2018-03-14 10:53:55 -0600

    users modeled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+
+  validates :screen_name, presence: true, length: { maximum: 30 }
+                          uniqueness: true
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 5 }
 end
