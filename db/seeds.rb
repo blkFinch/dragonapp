1fba2ac3da56841fe596f8acc4270820232706c6
N. Hutchison <blkfinch@gmail.com>
2018-03-21 13:33:38 -0600

    added bootstrap- refactored home index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(screen_name:"finch", password:"finchpass", 
+			password_confirmation:"finchpass", permission: 3)
+
+User.create(screen_name:"scout", password:"password", 
+			password_confirmation:"password", permission: 1)
+
+User.create(screen_name:"user", password:"password", 
+			password_confirmation:"password", permission: 1)
