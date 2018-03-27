61334ee849a92bfb9daf4931ab4520863723242c
N. Hutchison <blkfinch@gmail.com>
2018-03-27 12:41:41 -0600

    added edit

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,9 @@
 class Campaign < ApplicationRecord
   has_many :organizers
   has_many :users, :through => :organizers
+
+  validates :title, presence: true, 
+                    uniqueness: true
+
+  validates :description, presence: true 
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
   
+  it{ should validate_presence_of(:title) }
+  it{ should validate_uniqueness_of(:title) }
+  it{ should validate_presence_of(:description) }
+
+  it{ should have_many(:users).through(:organizers) }
 end
